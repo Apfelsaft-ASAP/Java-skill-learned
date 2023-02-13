29b9a3c517c3d8cd45667f3010014ee3e6fdf146
Myoung-Been <myoungbeen@gmail.com>
2023-02-13 21:04:26 +0900

    Report Update#1

--- Report2.java
+++ Report2.java
@@ -46,6 +46,7 @@ class Exercise3_3 {
         System.out.println((int)(Math.floor((float)num/100))*100);
     }
 }
+        // 나머지 구해서 뒤를 짜르면 됨
 ////예상 결과 -> 400
 //
 //
@@ -57,7 +58,7 @@ class Exercise3_4{
         int numOfApples = 123; // 사과의 개수
         int sizeOfBucket = 10; // 바구니의 크기(바구니에 담을 수 있는 사과의 개수)
         int numOfBucket = (int)((numOfApples/=sizeOfBucket)+Math.ceil((float)(numOfApples%=sizeOfBucket)/sizeOfBucket)); // 모든 사과를 담는데 필요한 바구니의 수
-
+        //int numOfBucket = numOfApples % sizeOfBucket == 0 ? numOfApples / sizeOfBucket : numOfApples / sizeOfBucket +1;
         System.out.println("필요한 바구니의 수 :"+numOfBucket);
     }
 }
@@ -82,5 +83,17 @@ class Exercise3_5{
 ////3-6. 아래는 화씨(Fahrenheit)를 섭씨(Celcius)로 변환하는 코드이다.
 ////변환 공식이 'C = 5/9*(F-32)'라고 할 때, 빈 칸에 알맞은 코드를 넣으시오.
 //// 단, 변환값은 소수점 셋째자리에서 반올림하며, Math.round() 함수를 사용하지 않고 처리할 것!
+class Exercise3_6{
+    public static void main(String[] args) {
+
+        int fahrenheit = 100;
+        float celcius = (int)((5/9f * (fahrenheit - 32)) * 100 + 0.5) / 100;
+
+        System.out.println("Fahrenheit:" + fahrenheit);
+        System.out.println("Celcius"+celcius);
+    }
+}
+
+
 
 ////예상 결과 : Fahrenheit:100, Celcius:37.78
