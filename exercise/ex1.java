177cd10cc79b3490237c6b3fc5446586ea38945b
Myoung-Been <myoungbeen@gmail.com>
2023-02-15 20:43:43 +0800

    day3 ex1 commit#1

--- exercise/ex1.java
+++ exercise/ex1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 //별(*)문자를 이용해 가로의 길이가 n,세로의 길이가 m인 직사각형 형태를 출력해보세요.
 //
 //제한조건 = n과 m은 각각 1000 이하인 자연수 입니다.
-class Solution {
+class ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
