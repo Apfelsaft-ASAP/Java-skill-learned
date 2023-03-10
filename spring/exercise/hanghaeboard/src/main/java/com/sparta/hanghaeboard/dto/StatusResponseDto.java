0b192d2013eb33fc89427555bb418b46f99706df
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-03-10 20:45:46 +0800

    회원가입, 로그인 구현#1 JWT토큰생성 #1 게시글 작성시 토큰 있을때막 작성 가능하게 구현#1 username 집어 넣었음#1 Update, Delete구현#1

---
+++ spring/exercise/hanghaeboard/src/main/java/com/sparta/hanghaeboard/dto/StatusResponseDto.java
@@ -0,0 +1,16 @@
+package com.sparta.hanghaeboard.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class StatusResponseDto {
+    private String msg;
+    private int statusCode;
+
+    public StatusResponseDto(String msg, int statusCode) {
+        this.msg = msg;
+        this.statusCode = statusCode;
+    }
+}
