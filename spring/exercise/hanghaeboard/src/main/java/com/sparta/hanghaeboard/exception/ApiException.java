86af81e178e3d7d42d5da1483f8e7865f26387de
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-03-17 15:37:16 +0800

    시큐리티 적용#2

---
+++ spring/exercise/hanghaeboard/src/main/java/com/sparta/hanghaeboard/exception/ApiException.java
@@ -0,0 +1,11 @@
+package com.sparta.hanghaeboard.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiException extends RuntimeException{
+
+    private final ExceptionEnum errorCode;
+}
